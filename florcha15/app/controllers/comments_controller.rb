7ff97928c3aaa359e0cb872ca3c99054bbb1082d
German Romarion <grromarion@hotmail.com>
2015-02-02 22:24:43 -0300

    Comments implementation done
    **Summary:** The Comment model has been fully implemented.

--- florcha15/app/controllers/comments_controller.rb
+++ florcha15/app/controllers/comments_controller.rb
@@ -1,7 +1,16 @@
 class CommentsController < ApplicationController
 	def new
+		@comments = Comment.all
 	end
 
 	def create
+		Comment.create!(comment_params)
+		redirect_to new_comment_path
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:username, :text)
 	end
 end
