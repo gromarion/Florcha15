43370eb8924d73bca89380e4e762b521ab54b7cf
German Romarion <grromarion@hotmail.com>
2015-02-28 14:40:16 -0300

    Owl carousel has been implemented
    **Summary:** An Owl carousel has been implemented in the header of the page.

--- florcha15/app/assets/javascripts/application.js
+++ florcha15/app/assets/javascripts/application.js
@@ -14,6 +14,19 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require foundation
+//= require owl.carousel
 //= require_tree .
 
+
 $(document).foundation();
+
+$(document).ready(function() {
+  var owl = $('#owl');
+  owl.owlCarousel({
+      items : 1, //1 items above 1000px browser width
+      itemsDesktop : [1000, 5], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900, 3], // betweem 900px and 601px
+      itemsTablet: [600, 2], //2 items between 600 and 0
+      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+  });
+});
