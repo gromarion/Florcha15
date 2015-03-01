6466d556e39051ab852ceb21748d44ba15b00b52
German Romarion <grromarion@hotmail.com>
2015-03-01 13:26:40 -0300

    Test responsive carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,6 +27,22 @@ $(document).ready(function() {
       itemsDesktop : [1000, 5], //5 items between 1000px and 901px
       itemsDesktopSmall : [900, 3], // betweem 900px and 601px
       itemsTablet: [600, 2], //2 items between 600 and 0
-      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+      itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+      loop: false,
+      responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:true
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
   });
 });
