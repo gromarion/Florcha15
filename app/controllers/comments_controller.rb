79414905b75b46896dc1d2b14edc8ca5e47c7ce9
German Romarion <grromarion@hotmail.com>
2015-03-01 17:09:11 -0300

    Corrected bug in comments creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,16 +11,12 @@ class CommentsController < ApplicationController
 		params[:comment][:text] = Obscenity.sanitize(params[:comment][:text])
 		Comment.create!(
 			username: current_user.full_name,
-			text: permit_comment_params
+			text: params[:comment][:text]
 		)
 		redirect_to new_comment_path
 	end
 
 	private
-
-	def permit_comment_params
-		params.require(:comment).permit(:text)
-	end
 
 	def fetch_comments
 		@comments = Comment.all.order(created_at: 'DESC')
